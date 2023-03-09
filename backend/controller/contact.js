0fd5655b7418c9d08f20051fcfbe92d1ead6f9f0
monil80 <patels.monil@gmail.com>
2023-03-09 14:21:18 +0530

    project submit

---
+++ backend/controller/contact.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+const Contact = require("../models/Contact");
+
+// Contacts Route
+router.get("/", (req, res) => {
+    res.json({ message: "This is the contact page" });
+  });
+
+
+  
+  router.post("/", (req, res) => {
+   // console.log(req.body)
+    const { fullName, email, message, city } = req.body;
+    let newContact = new Contact({
+      fullName, email, message, city
+    })
+    newContact.save();
+   // console.log("newContact has been saved")
+
+    res.json("contect detail submit")
+  
+  });
+  
+  // Stripe Integration Route
+
+
+module.exports = router;
+
