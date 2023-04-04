dbcd28cd582dca36f55fffcb811e84885b795003
monil <114733073+monil80@users.noreply.github.com>
2023-04-04 13:07:30 +0530

    Delete frontend directory

--- frontend/src/redux/constants/cartConstants.js
+++
@@ -1,6 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const CART_RESET = "CART_RESET";
-
-export const GET_TO_CART = "GET_TO_CART";
-export const LOGOUT_TO_CART = "GET_TO_CART";
